dd2a6eff575aa10d3d3d13e524e605985d28d4c4
Géza Búza <bghome@gmail.com>
2025-07-19 20:18:40 +0200

    Upgrade python-validity to 0.15
    Review the list of applied patches and remove those which are not needed.
    
    Most of them replaced the directory path used by python-validity to store
    data files. This has been changed upstream and now writes to the
    /var/run/python-validity/ directory which is writable on NixOS.
    
    Remove the unused tmpdir.py script.

--- pkgs/python-validity/default.nix
+++ pkgs/python-validity/default.nix
@@ -8,14 +8,14 @@
 in
   buildPythonPackage rec {
     pname = "python-validity";
-    version = "0.14";
+    version = "0.15";
     pyproject = true;
 
     src = fetchFromGitHub {
       owner = "uunicorn";
       repo = pname;
       rev = "${version}";
-      sha256 = "sha256-6NbxeokbGW5yP3g9Q/W3k0JiU6g+qyeZfKfw0nBJ37o="; # set to lib.fakeSha256 first to get the hash
+      sha256 = "sha256-RflX7e6nd11pSg8mh3mjZiVGNUSdox/SKXHR4W+PhMs="; # set to lib.fakeSha256 first to get the hash
     };
 
     build-system = with python3Packages; [
@@ -23,13 +23,6 @@ in
     ];
 
     patches = [
-      # the service writes to a temporary file in usr/share, which is write-protected in Nix.
-      # Instead, we let it write to the appropriate location for temporary files
-      ./dbus-service.patch
-
-      # similarly, state data needs to go to /var/lib
-      ./sensor.py.patch
-
       # rename dbus service executable
       ./python-validity-dbus-service.patch
 
@@ -37,27 +30,9 @@ in
       # Hence, we remove its declaration as a data file and declare it as a script instead, so that it is installed to /bin.
       # This is because the buildPythonPackage wrapper will only wrap the executable correctly if it is in the /bin directory
       ./setup.py.patch
-
-      # The firmware download script is run as part of the installation process on other distributions.
-      # However, it tries to identify the system hardware and download different firmware packages depending on the results.
-      #
-      # This sort of side-effect related behaviour is probably not ideal to have in the build process of a Nix package.
-      # Instead, the user shall call `validity-sensors-firmware` themselves when using python validity for the first time.
-      # TODO: We should make it so, that the python-validity service downloads the firmware automatically when not present.
-      #
-      # Hence, we patch the firmware download script so that it downloads the firmware to a temporary directory, instead of trying
-      # to write to /usr/share or the Nix store
-      ./validity-sensors-firmware.patch
-
-      # Because of the above firmware downloading patch, we also have patch the firmware search location in the script which
-      # performs the firmware installation
-      ./upload_fwext.py.patch
-    ];
+   ];
 
     postPatch = ''
-      # add support for using a temporary directory, which is needed for multiple of the above patches
-      cp ${./tmpdir.py} validitysensor/tmpdir.py
-
       # the firmware download script depends on innoextract
       substituteInPlace bin/validity-sensors-firmware \
             --replace "'innoextract'" \
